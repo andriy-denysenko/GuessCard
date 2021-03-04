b6acb57e95fbaf6cd252a1cf92819791802209ae
andriy-denysenko <denysenko.andriy@gmail.com>
2021-03-04 11:41:18 +0200

    Added selector buttons

--- app/gc.py
+++ app/gc.py
@@ -12,35 +12,37 @@ if __name__ == '__main__':
 
     d = Deck()
      
-    root = tkinter.Tk()
+    root = tkinter.Tk(className='Guess Card Window')
+    root.configure(bg='#ffffff')
      
-    # создаем рабочую область
-    frame = tkinter.Frame(root, bg = '#ffffff')
-    frame.grid()
+    # Create a space for an image
+    # frame = tkinter.Frame(root, bg = '#ffffff')
+    # frame.grid()
      
-    #Добавим изображение
+    # Add an image of card back
     canvas = tkinter.Canvas(root, height=300, width=200, bg = '#ffffff')
-    # img = tkinter.PhotoImage(file = f'./img/{d.pick()}.png') 
     img = tkinter.PhotoImage(file = os.path.join(script_dir, 'img', 'back.png'))
     image = canvas.create_image(0, 0, anchor='nw',image=img)
-    canvas.grid(row=0,column=0, columnspan=2)
+    canvas.grid(row=0, column=0, columnspan=7)
 
+    btn_suit_prev = tkinter.Button(root, text = '<')
+    btn_suit = tkinter.Button(root, text = '-')
+    btn_suit_next = tkinter.Button(root, text = '>')
+
+    btn_suit_prev.grid(row=1, column=0)
+    btn_suit.grid(row=1, column=1)
+    btn_suit_next.grid(row=1, column=2)
+
+    btn_rank_prev = tkinter.Button(root, text = '<')
+    btn_rank = tkinter.Button(root, text = '-')
+    btn_rank_next = tkinter.Button(root, text = '>')
+
+    btn_rank_prev.grid(row=1, column=4)
+    btn_rank.grid(row=1, column=5)
+    btn_rank_next.grid(row=1, column=6)
+
+    # TODO: we can shuffle the deck or pop a card AFTER a user pressed Go
     d.shuffle()
-
-    cbo_ranks = ttk.Combobox(root, height = 13)
-    rank_list = []
-    for r in RANKS:
-        rank_list.append(r)
-    cbo_ranks['values'] = rank_list
-
-    cbo_ranks.grid(row = 1, column = 0)
-
-    # btn_reset = tkinter.Button(root, text = 'Reset')
-    # btn_reset.grid(row=1, column=0)
-
-    # btn_try = tkinter.Button(root, text = 'Try')
-    # btn_try.grid(row=1, column=1)
-
 
 
     root.mainloop()
